b4d8e27873359947ed0dee7f39d3cc2718996244
Nayron Hollupi <nhollupi@gmail.com>
2022-03-09 09:42:06 -0300

    Adicionar arquivos de projeto.

---
+++ Numero_De_Dias/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Numero_De_Dias
+{
+    internal class DateTimeOffset
+    {
+
+        static void Main(string[] args)
+        {
+             
+            Console.WriteLine("Digite a primeira data 00/00/0000 :");
+            DateTime date1 = DateTime.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Digite a segunda data 00/00/0000 :");
+            DateTime date2 = DateTime.Parse(Console.ReadLine());
+
+            if (date1 < date2)
+            {
+
+                Console.WriteLine(date2 - date1);
+            }
+            else
+            {
+                Console.WriteLine(date1 - date2);
+            }
+   
+        }
+        }
+    }
+
